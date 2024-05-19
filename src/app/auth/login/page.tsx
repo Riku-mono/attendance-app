4dc4fdd11d2ad1f7b9dc3bc9897b067d5da6cf17
Riku-mono <riitkou@gmail.com>
2024-05-19 21:41:28 +0900

    chore: Add login and logout pages

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,51 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { signIn } from '@/auth'
+import { providerMap } from '@/auth.config'
+import { Button } from '@/components/ui/button'
+
+export default async function SignInPage() {
+  return (
+    <div className="relative mx-auto grid w-full max-w-7xl flex-1 grid-cols-1 lg:grid-cols-2">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Sign in</h1>
+            <p className="text-balance text-muted-foreground">Please sign in to continue</p>
+          </div>
+          <div className="grid gap-4">
+            {Object.values(providerMap).map((provider) => (
+              <form
+                action={async () => {
+                  'use server'
+                  await signIn(provider.id)
+                }}
+                className="space-y-8"
+                key={provider.id}
+              >
+                <Button type="submit" className="w-full">
+                  Sign in with {provider.name}
+                </Button>
+              </form>
+            ))}
+          </div>
+          <Button variant="outline">
+            <Link href="/">Cancel</Link>
+          </Button>
+          <div className="mt-4 text-center text-sm">
+            <p>Only acsess Horizon Members</p>
+          </div>
+        </div>
+      </div>
+      <div className="hidden max-h-[calc(100vh_-_theme(spacing.36))] overflow-visible lg:flex">
+        <Image
+          src="/images/login.png"
+          alt="Image"
+          width="1080"
+          height="1080"
+          className="my-auto max-w-full object-cover dark:brightness-[0.7]"
+        />
+      </div>
+    </div>
+  )
+}
---
+++ src/app/auth/logout/page.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import { signOut } from '@/auth'
+
+export default function LogoutPage() {
+  return (
+    <div className="relative mx-auto grid w-full max-w-7xl flex-1 grid-cols-1 lg:grid-cols-2">
+      <div className="flex items-center justify-center py-12">
+        <div className="mx-auto grid w-[350px] gap-6">
+          <div className="grid gap-2 text-center">
+            <h1 className="text-3xl font-bold">Sign out</h1>
+            <p className="text-balance text-muted-foreground">Are you sure you want to sign out?</p>
+          </div>
+          <div className="grid gap-4">
+            <form
+              action={async () => {
+                'use server'
+                await signOut()
+              }}
+              className="space-y-8"
+            >
+              <Button type="submit" className="w-full">
+                Sign out
+              </Button>
+            </form>
+            <Button variant="outline">
+              <Link href="/">Cancel</Link>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
